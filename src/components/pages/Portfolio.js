50408358270c39780dd97637ab35479bbf04aa93
Andy MCA <AndrewMcAlpin@gmail.com>
2022-06-07 18:04:06 -0500

    update

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Program from '../Program'
 
 export default function Portfolio() {
   return (
     <div>
         <h1>Portfolio</h1>
+        <Program />
     </div>
   );
 }
